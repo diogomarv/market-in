e1ddfb7fa85052c6e98181cd154272285f3a5d72
diogomarv <diogomarv@gmail.com>
2021-10-05 23:12:51 -0300

    User-registration page inserted

--- Front-end/src/app/app-routing.module.ts
+++ Front-end/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserLoginComponent } from './user-auth/user-login/user-login.component';
+import { UserRegistrationComponent } from './user-auth/user-registration/user-registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: UserLoginComponent},
+  {path: 'user-registration', component: UserRegistrationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+ }
